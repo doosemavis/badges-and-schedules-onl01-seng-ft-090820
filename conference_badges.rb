272d2a1fcaaa861ea1f5ed6088f573e455c1677d
Moose Davis <moosedavis2011@gmail.com>
2020-09-12 22:03:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,8 @@
+array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end 
-
-badge_maker("Ariel")
 
 
 def batch_badge_creator(array)
