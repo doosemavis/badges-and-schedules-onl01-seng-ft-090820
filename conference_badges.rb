a580e1ea4c8f2350d7d480458c247382cc5feb79
Moose Davis <moosedavis2011@gmail.com>
2020-09-12 22:27:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 
 
 def assign_rooms(speaker, room_number)
-  speaker.each do |speaker|, |room_number|
+  speaker.each do |speaker|
   puts "Hello #{speaker}!  You'll be assigned to room #{room_number}!"
 end 
 
