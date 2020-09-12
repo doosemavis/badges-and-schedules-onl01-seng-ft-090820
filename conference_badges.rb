f540ccc484ce1472fb6d7db7b68f110359a1ef96
Moose Davis <moosedavis2011@gmail.com>
2020-09-12 23:19:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,21 @@
 speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end 
+
 def batch_badge_creator(speaker)
 end
+
 def assign_rooms(room)
   speaker.each do |speaker|
   "Hello #{speaker}!  You'll be assigned to room #{room}!"
+  end
 end
-end 
+
 def printer
   puts 
-end 
+end
+
+
 badge_maker("Arel")
