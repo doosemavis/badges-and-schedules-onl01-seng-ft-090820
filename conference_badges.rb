5d9492d85d63f53b3e0a214f53c84e0b699aaebb
Moose Davis <moosedavis2011@gmail.com>
2020-09-12 21:54:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,7 @@ def batch_badge_creator(array)
   array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 end
 
+
 def assign_rooms(speaker, room_number)
   puts "Hello #{speaker}!  You'll be assigned to room #{room_number}!"
 end 
