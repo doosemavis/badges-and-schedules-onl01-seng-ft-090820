eb7a26e75590da638110622e1f8feeee6a3755c6
Moose Davis <moosedavis2011@gmail.com>
2020-09-12 22:11:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 room_number = [1, 2, 3, 4, 5, 6, 7]
 
 def badge_maker(name)
