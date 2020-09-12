6dfb34213ae6ac75b7c7adc511aa4ff95a751164
Moose Davis <moosedavis2011@gmail.com>
2020-09-12 23:24:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,14 +4,17 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 
+
 def batch_badge_creator(speaker)
 end
+
 
 def assign_rooms(room)
   speaker.each do |speaker|
   "Hello #{speaker}!  You'll be assigned to room #{room}!"
   end
 end
+
 
 def printer
   puts 
